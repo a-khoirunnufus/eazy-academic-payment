947456ed8dd6581bbad9f36820138c00e7f9c546
Imron Abu Laiz <ronabulaiz@gmail.com>
2023-03-20 16:36:39 +0700

    config: Read Write Database

--- config/database.php
+++ config/database.php
@@ -65,8 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
+            'read' => [
+                'host' => [
+                    env('DB_HOST_READ', '127.0.0.1')
+                ]
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_HOST_WRITE', '127.0.0.1')
+                ]
+            ],
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
