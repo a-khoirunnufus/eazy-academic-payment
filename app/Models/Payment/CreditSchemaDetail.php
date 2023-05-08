56059ad3733d496921320fc13e4af03885bd124b
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-05-08 15:44:12 +0700

    feat(credit-schema): add soft delete feature

--- app/Models/Payment/CreditSchemaDetail.php
+++ app/Models/Payment/CreditSchemaDetail.php
@@ -4,10 +4,11 @@ namespace App\Models\Payment;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreditSchemaDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = "finance.credit_schema_detail";
 
