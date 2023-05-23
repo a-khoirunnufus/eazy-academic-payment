9652b5ca8d9241720ebde6fea8a4fbf231910762
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-05-23 19:41:20 +0700

    progress import setting fee

---
+++ resources/views/exports/component-fee.blade.php
@@ -0,0 +1,61 @@
+<table>
+    <tbody>
+        <!-- SHEET HEADER -->
+        <tr>
+            <td><strong>{{ $title }}</strong></td>
+        </tr>
+
+        <!-- EMPTY ROW -->
+        <tr><td></td></tr>
+
+        <tr>
+            <td>Tahun Ajaran: {{ $academic_year }}</td>
+        </tr>
+        <tr>
+            <td>Periode: {{ $period }}</td>
+        </tr>
+        <tr>
+            <td>Jalur: {{ $path }}</td>
+        </tr>
+        <tr>
+            <td>Program Studi: {{ $studyprogram }}</td>
+        </tr>
+        <tr>
+            <td>Jenis Perkuliahan: {{ $lecture_type }}</td>
+        </tr>
+        <tr>
+            <td>Jangan ubah kode berikut: {{ $encrypted_string }}</td>
+        </tr>
+
+        <!-- EMPTY ROW -->
+        <tr><td></td></tr>
+
+        <tr>
+            <td>Petunjuk Pengisian Data:</td>
+        </tr>
+        @foreach($guides as $guide)
+            <tr>
+                <td>{{ $guide }}</td>
+            </tr>
+        @endforeach
+
+        <!-- EMPTY ROW -->
+        <tr><td></td></tr>
+
+        <!-- TABLE HEAD -->
+        <tr>
+            @foreach($data[0] as $key => $value)
+                <td><strong>{{ $key }}</strong></td>
+            @endforeach
+        </tr>
+
+        <!-- TABLE BODY -->
+        @foreach($data as $row)
+            <tr>
+                @foreach ($row as $value)
+                    <td>{{ $value }}</td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+</table>
