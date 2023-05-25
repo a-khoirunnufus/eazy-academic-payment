5a42e8c808f398585c392507ca1dfa879956f807
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-05-26 01:53:37 +0700

    feat: import setting fee using excel

--- app/Models/Payment/CreditSchema.php
+++ app/Models/Payment/CreditSchema.php
@@ -2,9 +2,12 @@
 
 namespace App\Models\Payment;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Log;
+use App\Models\Scopes\CreditSchemaTemplateScope;
 
 class CreditSchema extends Model
 {
@@ -14,10 +17,20 @@ class CreditSchema extends Model
 
     protected $primaryKey = 'cs_id';
 
-    protected $fillable = ['cs_name', 'cs_valid'];
+    protected $fillable = ['cs_name', 'cs_valid', 'is_template'];
 
     public function creditSchemaDetail()
     {
         return $this->hasMany(CreditSchemaDetail::class, 'csd_cs_id', 'cs_id')->orderBy('csd_order')->with('creditSchemaDeadline');
+    }
+
+    public function creditSchemaPeriodPath()
+    {
+        return $this->hasMany(CreditSchemaPeriodPath::class, 'cs_id', 'cs_id');
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new CreditSchemaTemplateScope);
     }
 }
