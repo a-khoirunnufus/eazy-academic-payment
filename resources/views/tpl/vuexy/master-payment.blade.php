10faf9d1739f7d1f87da9519d50cc022ef62410f
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-09-15 11:32:32 +0700

    update(payment-layout): add 'styles' blade stack

--- resources/views/tpl/vuexy/master-payment.blade.php
+++ resources/views/tpl/vuexy/master-payment.blade.php
@@ -44,6 +44,7 @@
     <link rel="stylesheet" type="text/css" href="{{ url('css/custom.css') }}?version={{ config('version.css_style') }}">
 
     @yield('css_section')
+    @stack('styles')
 </head>
 
 <body class="vertical-layout vertical-menu-modern  navbar-floating footer-static menu-@yield('sidebar-size')" data-open="click" data-menu="vertical-menu-modern" data-col="">
